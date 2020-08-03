51062e04dbba8c923a62801fed11cd741fc13fcf
= <=>
2020-08-04 01:27:01 +0530

    Wrote the main program; intialized cart and rules; program prints output perfectly

--- Promotion/PromotionMain/Program.cs
+++ Promotion/PromotionMain/Program.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using PromotionMain.Models;
+using PromotionMain.Rules;
+using System.Linq;
 
 namespace PromotionMain
 {
     public class Program
     {
+        static List<SKU> fixedSKUs;
+        static List<IRule> rules;
+        static List<char> initialCart;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            fixedSKUs = PopulateSKUs();
+            IntializeScenario();
+
+            var cart = initialCart;
+            Console.WriteLine("Promotion rules applied: ");
+            foreach(var rule in rules)
+            {
+                while(rule.IsApplicable(cart))
+                {
+                    cart = rule.Apply(cart);
+                    Console.WriteLine(rule.getPrettyMessage());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Individual Items");
+            foreach(var item in cart)
+            {
+                Console.WriteLine("Individual "+item+" for "+fixedSKUs.Where(x => x.letter == item).First().price);
+            }
+
         }
 
         public static List<SKU> PopulateSKUs()
@@ -20,6 +46,21 @@ namespace PromotionMain
             list.Add(new SKU {letter='D',price=15});
 
             return list;
+        }
+
+        public static void IntializeScenario()
+        {
+            rules = new List<IRule>();
+            rules.Add(new Rule1('A',3,130));
+            rules.Add(new Rule2('B','C',30));
+
+            initialCart = new List<char>();
+            initialCart.Add('A');
+            initialCart.Add('A');
+            initialCart.Add('A');
+            initialCart.Add('A');
+            initialCart.Add('B');
+            initialCart.Add('C');
         }
     }
 }
